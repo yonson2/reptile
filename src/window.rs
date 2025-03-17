2d1e585527f4a774214236e2ad630d5a7846b2a3
Peter <s@pramos.me>
2025-03-17 12:59:40 +0100

    wasm fixes

--- src/window.rs
+++ src/window.rs
@@ -1,21 +1,30 @@
-use bevy::{log::LogPlugin, prelude::*, window::WindowResolution};
+use bevy::{asset::AssetMetaCheck, log::LogPlugin, prelude::*, window::WindowResolution};
 
 pub(super) fn plugin(app: &mut App) {
-    app.add_plugins(
-        DefaultPlugins
-            .set(WindowPlugin {
-                primary_window: Some(Window {
-                    resolution: WindowResolution::new(800., 1600.).with_scale_factor_override(1.0),
-                    resizable: false,
-                    ..default()
-                }),
+    let mut plugins = DefaultPlugins
+        .set(WindowPlugin {
+            primary_window: Some(Window {
+                resolution: WindowResolution::new(800., 1600.).with_scale_factor_override(1.0),
+                resizable: false,
                 ..default()
-            })
-            .set(ImagePlugin::default_nearest())
-            .set(LogPlugin {
-                filter: "info,wgpu_core=warn,wgpu_hal=warn,mygame=debug".into(),
-                level: bevy::log::Level::DEBUG,
-                custom_layer: |_| None,
             }),
-    );
+            ..default()
+        })
+        .set(ImagePlugin::default_nearest())
+        .set(LogPlugin {
+            filter: "info,wgpu_core=warn,wgpu_hal=warn,mygame=debug".into(),
+            level: bevy::log::Level::DEBUG,
+            custom_layer: |_| None,
+        });
+
+    // Only set AssetPlugin for wasm32 target
+    #[cfg(target_arch = "wasm32")]
+    {
+        plugins = plugins.set(AssetPlugin {
+            meta_check: AssetMetaCheck::Never,
+            ..default()
+        });
+    }
+
+    app.add_plugins(plugins);
 }
